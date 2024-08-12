02d260feef351c1e2c003e695f4bb80272d73705
taiyang <dakaitaiyang@gmail.com>
2024-08-12 22:02:50 +0800

    speed-mall项目的创建

---
+++ speed-mall/src/main/java/com/example/speedmall/SpeedMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.speedmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedMallApplication.class, args);
+    }
+
+}
